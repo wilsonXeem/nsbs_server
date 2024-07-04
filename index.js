7c633e91edc8ad0611cd87dbf55c8be1081b1220
Wilson Zimthamaha Bonkuru <66775520+wilsonXeem@users.noreply.github.com>
2024-07-04 17:36:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://nsbs-2024.vercel.app/",
   },
 });
 
